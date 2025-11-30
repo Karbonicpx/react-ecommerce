ff09ff2a75ac6a304ba223f6afcb97eb90475135
Yatagaru <nap1903@gmail.com>
2025-11-30 15:54:13 -0300

    Implementing Checkout and Fixing Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import './index.css'
 import HomePage from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
 import { Routes, Route } from "react-router"; // Used to make a page be inserted in the routing (SPA)
+
 
 function App() {
 
@@ -9,7 +12,7 @@ function App() {
         <Routes>
             {/* Route path (URL, in this case is empty so index, or "/") is basically the page, and element is the component we want to show in that page */}
             <Route index element={<HomePage />}/>
-            <Route path="checkout" element= {<div>Test Checkout Page</div>}/>
+            <Route path="checkout" element= {<CheckoutPage/>}/>
         </Routes>
 
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom/client"; // Required for rendering the root
-import HomePage from "./pages/HomePage"
+import App from "./App";
 import { BrowserRouter } from "react-router"; // Used to navigate in different pages with the same html
 
 // Render the root, which is the main component that holds all other components
 ReactDOM.createRoot(document.getElementById("root")).render((
   <>
+    {/* Basically saying that the homePage is inside the routing in the App */}
     <BrowserRouter>
-      <HomePage />
+     <App/>
     </BrowserRouter>
   </>
 ))
