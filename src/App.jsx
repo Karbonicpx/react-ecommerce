8386007ba546779892fbc86076d696bffb1f07d1
Yatagaru <nap1903@gmail.com>
2025-11-30 15:22:38 -0300

    Implementing Routing in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router"; // Used to make a page be inserted in the routing (SPA)
 
-function App(){
-    
+function App() {
+
     return (
-        <HomePage/>
+
+        // Need to wrap the elements in the Routes to work
+        <Routes>
+            {/* Route path (URL, in this case is empty so index, or "/") is basically the page, and element is the component we want to show in that page */}
+            <Route index element={<HomePage />}/>
+            <Route path="checkout" element= {<div>Test Checkout Page</div>}/>
+        </Routes>
+
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client"; // Required for rendering the root
 import HomePage from "./pages/HomePage"
+import { BrowserRouter } from "react-router"; // Used to navigate in different pages with the same html
 
 // Render the root, which is the main component that holds all other components
 ReactDOM.createRoot(document.getElementById("root")).render((
   <>
-    <HomePage/>
-
+    <BrowserRouter>
+      <HomePage />
+    </BrowserRouter>
   </>
 ))
