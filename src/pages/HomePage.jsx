022ccf5b306f31d57557cbfa279574a6190e80d4
Yatagaru <nap1903@gmail.com>
2025-12-01 18:53:39 -0300

    Implementing Backend in products-grid

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,160 +1,94 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './HomePage.css';
 import { Header } from '../components/Header';
 export function HomePage() {
 
+    /* Getting the products backend data, with a fetch request
+    // Returns a promise --> Asynchronous code
+    fetch('http://localhost:3000/api/products')
+    .then((response) =>{ // When it finishes fetching, loads the "then"
+
+        // Transforming the response to a json, and when this process finishes executes the next "then"
+        return response.json().then(() => {
+
+        })
+    })
+    */
+
+    // Saving the data of the backend in this state ([] is for empty initial value)
+    const [products, setProducts] = useState([]);
+    // Using a useEffect since the request needs to only run once (with empty array)
+    useEffect(() => {
+
+        // OR, you can use axios (npm package) which already makes the json conversion process
+        axios.get("http://localhost:3000/api/products").then((response) => {
+
+           setProducts(response.data)
+        })
+    }, [])
+
     // Loading the elements of the / into this component
     return (
+
+
         <>
             <Header />
 
             <div className="home-page">
                 <div className="products-grid">
-                    <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                                src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-                        </div>
+                    {products.map((product) => {
+                        return (
+                            <div key={product.id} className="product-container">
+                                <div className="product-image-container">
+                                    <img className="product-image"
+                                        src={product.image} />
+                                </div>
 
-                        <div className="product-name limit-text-to-2-lines">
-                            Black and Gray Athletic Cotton Socks - 6 Pairs
-                        </div>
+                                <div className="product-name limit-text-to-2-lines">
+                                    {product.name}
+                                </div>
 
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                                src="images/ratings/rating-45.png" />
-                            <div className="product-rating-count link-primary">
-                                87
+                                <div className="product-rating-container">
+                                    <img className="product-rating-stars"
+                                        src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
+                                    <div className="product-rating-count link-primary">
+                                        {product.rating.count}
+                                    </div>
+                                </div>
+
+                                <div className="product-price">
+                                    {(product.priceCents / 100).toFixed(2)}
+                                </div>
+
+                                <div className="product-quantity-container">
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                </div>
+
+                                <div className="product-spacer"></div>
+
+                                <div className="added-to-cart">
+                                    <img src="images/icons/checkmark.png" />
+                                    Added
+                                </div>
+
+                                <button className="add-to-cart-button button-primary">
+                                    Add to Cart
+                                </button>
                             </div>
-                        </div>
-
-                        <div className="product-price">
-                            $10.90
-                        </div>
-
-                        <div className="product-quantity-container">
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                        </div>
-
-                        <div className="product-spacer"></div>
-
-                        <div className="added-to-cart">
-                            <img src="images/icons/checkmark.png" />
-                            Added
-                        </div>
-
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                    </div>
-
-                    <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                                src="images/products/intermediate-composite-basketball.jpg" />
-                        </div>
-
-                        <div className="product-name limit-text-to-2-lines">
-                            Intermediate Size Basketball
-                        </div>
-
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                                src="images/ratings/rating-40.png" />
-                            <div className="product-rating-count link-primary">
-                                127
-                            </div>
-                        </div>
-
-                        <div className="product-price">
-                            $20.95
-                        </div>
-
-                        <div className="product-quantity-container">
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                        </div>
-
-                        <div className="product-spacer"></div>
-
-                        <div className="added-to-cart">
-                            <img src="images/icons/checkmark.png" />
-                            Added
-                        </div>
-
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                    </div>
-
-                    <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                                src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-                        </div>
-
-                        <div className="product-name limit-text-to-2-lines">
-                            Adults Plain Cotton T-Shirt - 2 Pack
-                        </div>
-
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                                src="images/ratings/rating-45.png" />
-                            <div className="product-rating-count link-primary">
-                                56
-                            </div>
-                        </div>
-
-                        <div className="product-price">
-                            $7.99
-                        </div>
-
-                        <div className="product-quantity-container">
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                        </div>
-
-                        <div className="product-spacer"></div>
-
-                        <div className="added-to-cart">
-                            <img src="images/icons/checkmark.png" />
-                            Added
-                        </div>
-
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                    </div>
+                        )
+                    })}
                 </div>
             </div>
         </>
